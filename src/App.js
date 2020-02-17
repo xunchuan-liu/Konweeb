6f08ef1737442051664f16686ce3badec6c5064c
therodrammer <39782635+therodrammer@users.noreply.github.com>
2020-02-17 00:02:10 -0600

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>konweeb</h1>
+      <h3>Feedback</h3>
+      <img src={require('./flag.jpg')} alt="Italian Flag"/>
+      
+    </div>
+  );
+}
+
+export default App;
